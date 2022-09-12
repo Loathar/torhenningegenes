e74c9ca34da7cf5d73b17b9f64d8579e3c41b067
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-09-12 15:49:08 +0200

    some progress

--- pages/prosjekter.js
+++ pages/prosjekter.js
@@ -2,18 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import torhenning from "../img/torhenning.webp";
 import tellelisten from "../img/tellelistenSM.png";
+import kvistbyegenes from "../img/kvistbyegenesSM.png";
 export default function Prosjekter() {
   return (
     <section
-      className="prosjekter text-gray-700 mt-11 w-96 flex flex-col items-center justify-center mx-auto"
+      className="prosjekter text-gray-700 mt-11 flex flex-col items-center justify-center mx-auto"
       id="prosjekter"
     >
       <h1 className="text-center text-2xl uppercase ">Siste prosjekter</h1>
-      <div className="flex flex-row justify-around bg-pink-300 items-center">
-        <div className="m-4 min-w-min">
+      <div className="flex  flex-col md:flex-row justify-around bg-pink-300 items-center max-w-3xl">
+        <div className="project w-96 h-96 m-2">
           <h1>Telleliste for barnehage</h1>
 
-          <Image src={tellelisten} className="rounded-2xl " />
+          <Image src={tellelisten} className="rounded-2xl" />
           <div className="flex flex-row justify-between items-center align-middle">
             <Link href="https://tellelisten.netlify.app">
               <button className="bg-gray-900 text-gray-50 px-16 py-3 hover:bg-pink-600 hover:text-gray-50 transition-colors duration-300 ease-out mr-5 uppercase">
@@ -29,11 +30,23 @@ export default function Prosjekter() {
           </div>
         </div>
 
-        <div className="m-4">
-          <h1>Kvist by Egenes</h1>{" "}
-        </div>
-        <div className="m-4">
-          <h1>Statisk side</h1>{" "}
+        <div className="project w-96 h-96 m-2">
+          <h1>Kvist by Egenes</h1>
+
+          <Image src={kvistbyegenes} className="rounded-2xl " />
+          <div className="flex flex-row justify-between items-center align-middle">
+            <Link href="https://tellelisten.netlify.app">
+              <button className="bg-gray-900 text-gray-50 px-16 py-3 hover:bg-pink-600 hover:text-gray-50 transition-colors duration-300 ease-out mr-5 uppercase">
+                Live
+              </button>
+            </Link>
+
+            <Link href="https://github.com/Loathar/telleliste">
+              <button className="bg-gray-900 text-gray-50 px-16 py-3 hover:bg-pink-600 hover:text-gray-50 transition-colors duration-300 ease-out mr-5 uppercase">
+                Kode
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
