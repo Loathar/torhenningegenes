6226806564575d46aa394db1bd745e39f5242a92
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-09-13 07:58:35 +0200

    added githublogo to buttons

--- pages/prosjekter.js
+++ pages/prosjekter.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import torhenning from "../img/torhenning.webp";
 import tellelisten from "../img/tellelistenSM.png";
 import kvistbyegenes from "../img/kvistbyegenesSM.png";
+import githubLogo from "../img/GitHub-32px.png";
 export default function Prosjekter() {
   return (
     <section
@@ -23,8 +24,18 @@ export default function Prosjekter() {
             </Link>
 
             <Link href="https://github.com/Loathar/telleliste">
-              <button className="bg-gray-900 text-gray-50 px-16 py-3 hover:bg-pink-600 hover:text-gray-50 transition-colors duration-300 ease-out mr-5 uppercase">
-                Kode
+              <button className="bg-gray-900 text-gray-50   px-14 py-4 hover:bg-pink-600 hover:text-gray-50 transition-colors duration-300 ease-out mr-5 uppercase">
+                <div className="flex flex-row align-middle items-center justify-between">
+                  <p className="mr-3">Kode</p>
+                  <div className="h-8 w-8">
+                    <Image
+                      src={githubLogo}
+                      className="relative"
+                      layout="responsive"
+                      style={{ position: "relative" }}
+                    />
+                  </div>
+                </div>
               </button>
             </Link>
           </div>
@@ -42,8 +53,18 @@ export default function Prosjekter() {
             </Link>
 
             <Link href="https://github.com/Loathar/telleliste">
-              <button className="bg-gray-900 text-gray-50 px-16 py-3 hover:bg-pink-600 hover:text-gray-50 transition-colors duration-300 ease-out mr-5 uppercase">
-                Kode
+              <button className="bg-gray-900 text-gray-50   px-14 py-4 hover:bg-pink-600 hover:text-gray-50 transition-colors duration-300 ease-out mr-5 uppercase">
+                <div className="flex flex-row align-middle items-center justify-between">
+                  <p className="mr-3">Kode</p>
+                  <div className="h-8 w-8">
+                    <Image
+                      src={githubLogo}
+                      className="relative"
+                      layout="responsive"
+                      style={{ position: "relative" }}
+                    />
+                  </div>
+                </div>
               </button>
             </Link>
           </div>
