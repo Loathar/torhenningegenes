a2466c1bcfd0c615b82043f3678bb6400745a02a
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-09-19 08:58:53 +0200

    added some more logos

--- pages/about.js
+++ pages/about.js
@@ -6,12 +6,10 @@ import jsLogo from "../img/logo/javascript.png";
 import reactLogo from "../img/logo/react.png";
 import nextLogo from "../img/logo/nextjs.png";
 import tailwindLogo from "../img/logo/tailwind.png";
-import npmLogo from "../img/logo/npm.png";
+import tsLogo from "../img/logo/typescript.png";
 import firebaseLogo from "../img/logo/firebase.png";
 
 export default function About() {
-  console.log("hei");
-
   return (
     <>
       <section className="container mx-auto text-center flex flex-col items-center text-gray-900 min-h-screen mt-20 mb-28">
@@ -23,15 +21,17 @@ export default function About() {
           <div className=" w-72 h-72  mb-4 m-2">
             <Image src={torhenningPortrett} layout="responsive" />
           </div>
-          <p className="text-gray-900 mb-24 h-80 w-80 m-2 ">
-            Her skal det stå fornuftig tekst om drømmer håp og visjoner. Litt om
-            læring. koding og annet smart. quis deserunt ullamco nostrud
-            laborum. Adipisicing anim ad irure enim excepteur consequat. Tempor
-            qui ullamco dolor id laboris excepteur ea irure voluptate nulla
-            tempor elit nisi consequat. Occaecat occaecat aliqua sint elit
-            labore minim exercitation exercitation exercitation. Commodo anim
-            pariatur incididunt Lorem dolor sunt esse aliquip.
-          </p>
+          <div className="flex flex-col mb-24 h-80 w-80 m-2 leading-relaxed ">
+            <p className=" ">
+              Jeg er 37 år gammel sørlending bosatt i Oslo. Jeg har kodet siden
+              2020 og elsker det! Trives best når jeg utvikler web aplikasjoner
+              med NextJS og Tailwind CSS.
+            </p>
+            <span className="mt-1">
+              Ta kontakt hvis jeg kan hjelpe deg med noe!
+            </span>
+            <span className="mt-2  font-medium">Tor Henning</span>
+          </div>
         </div>
         <div className="mt-12">
           <h1 className="text-gray-900 text-3xl uppercase tracking-wider mt-16 mb-8">
@@ -39,32 +39,51 @@ export default function About() {
           </h1>
           <div className="">
             <ul className="flex flex-row justify-between flex-wrap text-center items-center align-middle">
-              <li className="m-4 w-16 h-16 ">
-                <Image
-                  src={htmlLogo}
-                  layout="responsive"
-                  className="grayscale hover:grayscale-0"
-                />
-                HTML
+              <li className="">
+                <div className="m-4 w-16 h-16 ">
+                  <Image
+                    src={htmlLogo}
+                    layout="responsive"
+                    className="grayscale hover:grayscale-0"
+                  />
+                </div>
+                <span className="">HTML</span>
               </li>
-              <li className="m-4  w-14 h-16 ">
-                <Image
-                  src={cssLogo}
-                  layout="responsive"
-                  className="grayscale hover:grayscale-0"
-                />
-                CSS
+
+              <li className="">
+                <div className="m-4  w-14 h-16 ">
+                  <Image
+                    src={cssLogo}
+                    layout="responsive"
+                    className="grayscale hover:grayscale-0"
+                  />
+                </div>
+                <span>CSS</span>
               </li>
-              <li className="m-4 w-20 h-16 ">
-                <Image
-                  src={jsLogo}
-                  layout="responsive"
-                  className="grayscale hover:grayscale-0"
-                />
+              <li>
+                <div className="m-4 w-20 h-16 ">
+                  <Image
+                    src={jsLogo}
+                    layout="responsive"
+                    className="grayscale hover:grayscale-0"
+                  />
+                </div>
                 JavaScript
               </li>
-              <li className="m-4 w-16 h-16 ">
-                <div>
+
+              <li>
+                <div className="m-4 w-20 h-16">
+                  <Image
+                    src={tsLogo}
+                    layout="responsive"
+                    className="grayscale hover:grayscale-0"
+                  />
+                </div>
+                TypeScript
+              </li>
+
+              <li>
+                <div className="m-4 w-16 h-16 ">
                   <Image
                     src={reactLogo}
                     layout="responsive"
@@ -73,8 +92,8 @@ export default function About() {
                 </div>
                 <span className="mt-2">ReactJS</span>
               </li>
-              <li className="m-4 w-16 h-16 ">
-                <div>
+              <li>
+                <div className="m-4 w-16 h-16 ">
                   <Image
                     src={nextLogo}
                     layout="responsive"
@@ -83,8 +102,9 @@ export default function About() {
                 </div>
                 <span className="mt-2">NextJS</span>
               </li>
-              <li className="m-4 w-16 h-16 ">
-                <div>
+
+              <li>
+                <div className="m-4 w-16 h-16">
                   <Image
                     src={tailwindLogo}
                     layout="responsive"
@@ -93,18 +113,9 @@ export default function About() {
                 </div>
                 <span className="mt-2">tailwind CSS</span>
               </li>
-              <li className="m-4 w-16 h-16 ">
-                <div>
-                  <Image
-                    src={npmLogo}
-                    layout="responsive"
-                    className="grayscale hover:grayscale-0 p-2 "
-                  />
-                </div>
-                <span className="mt-2">NPM</span>
-              </li>
-              <li className="m-4 w-14 h-14 ">
-                <div>
+
+              <li>
+                <div className="m-4 w-14 h-16">
                   <Image
                     src={firebaseLogo}
                     layout="responsive"
