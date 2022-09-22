3c784a9fa4cd85b5c068e598c54f8ea5277a7f4a
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-09-22 23:21:11 +0200

    small changes on about pages

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -7,11 +7,11 @@ export default function Kontakt() {
   return (
     <>
       <section className="flex flex-col items-center mt-28" id="kontakt">
-        <p>La oss snakke sammen!</p>
+        <p className="text-2xl mb-8">La oss snakke sammen!</p>
 
         <ul className="flex flex-col items-center">
           <li className="flex flex-row justify-around">
-            <span className="material-symbols-outlined text-gray-900 mr-2 ">
+            <span className="material-symbols-outlined text-gray-900 mr-2 h-6 w-6">
               mail
             </span>
             <span>tor.henning.egenes@gmail.com</span>
